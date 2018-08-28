549fca8bc302f2e565f6dcf2a1e22d9f92c18c40
qj <qujinxiong@aliyun.com>
2018-08-28 09:50:04 +0800

    remove some file

--- dist/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-const server = app.listen(3000, () => {
-    console.log('Server is running at http://localhost:3000');
-    console.log('Press CTRL-C to stop \n');
-});
-module.exports = server;
-//# sourceMappingURL=server.js.map
